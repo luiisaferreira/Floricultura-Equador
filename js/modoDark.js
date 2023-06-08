b9f34cada1addebe1e802f02f4ea8e764108a0b5
Isabela Silva <isabelasilvap778@gmail.com>
2023-06-08 00:04:37 -0300

    Atualização rodapé + modo dark

--- js/modoDark.js
+++ js/modoDark.js
@@ -72,8 +72,13 @@ darkMode.addEventListener('click', () => {
 
     /*Modo dark do rodapé*/
     const rodape = document.querySelector('.rodape')
-    
+    const telefone = document.querySelector('.iconeTelefone p')
+    const whats = document.querySelector('.iconeWhats p')
+    const instagram = document.querySelector('.iconeInstagram p')
 
     rodape.classList.toggle('dark')
+    telefone.classList.toggle('dark')
+    whats.classList.toggle('dark')
+    instagram.classList.toggle('dark')
 
 })
